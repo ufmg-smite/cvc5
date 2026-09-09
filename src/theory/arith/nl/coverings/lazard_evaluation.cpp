78ca7815e8c7fe924948a8feb4f668a7bd8135fb
Tomaz Mascarenhas <tomgm1502@gmail.com>
2026-09-09 17:16:10 -0300

    remove unused fn in lazard

--- src/theory/arith/nl/coverings/lazard_evaluation.cpp
+++ src/theory/arith/nl/coverings/lazard_evaluation.cpp
@@ -892,15 +892,6 @@ std::vector<poly::Value> LazardEvaluation::isolateRealRoots(
       << std::endl;
   return poly::isolate_real_roots(q, d_state->d_assignment);
 }
-std::vector<poly::Interval> LazardEvaluation::infeasibleRegions(
-    const poly::Polynomial& q, poly::SignCondition sc, std::vector<poly::Value> *rootsEmp) const
-{
-  WarningOnce()
-      << "nl-cov::LazardEvaluation is disabled because CoCoA is not available. "
-         "Falling back to regular calculation of infeasible regions."
-      << std::endl;
-  return poly::infeasible_regions(q, d_state->d_assignment, sc);
-}
 
 }  // namespace cvc5::internal::theory::arith::nl::coverings
 
